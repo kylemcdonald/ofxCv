e025f6b8b19ccb38521e323a629cda76ae994f2d
Antoine Villeret <antoine.villeret@gmail.com>
2015-10-05 11:47:06 +0200

    put example description in ofApp.cpp according to https://github.com/kylemcdonald/ofxCv/issues/157

--- example-blur/src/ofApp.cpp
+++ example-blur/src/ofApp.cpp
@@ -1,3 +1,18 @@
+/*
+# example-blur
+
+## Description
+This example demonstrates two different blur methods (gaussian and normalized box) and a method to compute the focus amount of an image.
+Focus computation is useful for objects (and especially iris) detection.
+You can find more about focus computation on the appendix of [*How Iris Recognition Works* by Paul Daugman](http://www.cl.cam.ac.uk/~jgd1000/csvt.pdf) (p.29).
+
+## Running
+Console displays computed focus amount.
+Use space bar to toggle between blur methods.
+Use 'a' and 'z' to de- / in-crease blur amount.
+See the result on focus computation.
+*/
+
 #include "ofApp.h"
 
 void ofApp::setup() {
